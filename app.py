40b3caf9ae7ffd69bdb5669f094e4177150d63a3
pandeyanubhavipdanand24m-stack <pandey.anubhavipdanand24m@iimranchi.ac.in>
2026-01-07 01:34:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mediterranean Masala Chips", layout="wide")
+
+st.title("Mediterranean Masala Chips")
+st.subheader("Bold Mediterranean flavor. Indian soul.")
+
+st.write("""
+Mild, balanced chips inspired by Mediterranean spices  
+Crafted for modern, health-conscious snackers
+""")
